49455ec89b8c187ec2cdb741b8f36484b4cd98b9
hackersky109 <hackersky109@gmail.com>
2016-12-04 15:41:52 +0800

    add crawl multipage

--- TripHobo/spiders/triphobo.py
+++ TripHobo/spiders/triphobo.py
@@ -14,38 +14,28 @@
 #             }
 
 import scrapy
+# scrapy crawl triphobo -o triphobo.json
 
-
+count = 0
 class TripSpider(scrapy.Spider):
     name = 'triphobo'
-
     start_urls = ['https://www.triphobo.com/tripplans/chicago']
 
     def parse(self, response):
+
         # follow links to author pages
         for href in response.css('.blocklist-trip-name-wrapper a::attr(href)').extract():
-            yield scrapy.Request(href,
-                                 callback=self.parse_plan)
+            yield scrapy.Request(href,callback=self.parse_plan)
 
         # follow pagination links
-        # next_page = response.css('li.next a::attr(href)').extract_first()
-        # if next_page is not None:
-        #     next_page = response.urljoin(next_page)
-        #     yield scrapy.Request(next_page, callback=self.parse)
+        global count
+        next_page = response.css('li.next a::attr(href)').extract_first()
+        if (next_page is not None) and (count<1) :
+            count=count+1
+            # next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
 
     def parse_plan(self, response):
-        # def extract_with_css(query):
-        #     return response.css(query).extract_first().strip()
-
-        # #'start-city': extract_with_css('.start-city-name span::text')
-        # yield {
-        #     'transit-city': extract_with_css('.transit-city span::text'),
-        # }
-
-        # title = response.xpath('//*[contains(@class, "step-2-itin-name")]/h1//text()')
-        # print "~~~~~~~~~~~~~~~~~~~\n"
-        # print response.xpath('//*[contains(@class, "step-2-itin-name")]/h1//text()')[0].extract()
-
         yield {
             # 'title': extract_with_css('.step-2-itin-name h1::text'),
             'title': response.xpath('//*[contains(@class, "step-2-itin-name")]/h1//text()|//*[contains(@class, "step-2-itin-name")]/h2//text()')[0].extract(),
